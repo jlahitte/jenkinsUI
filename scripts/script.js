51e120ad44076ddfe0d69b9c2b6520160ae361fd
Johann LAHITTE <jlahitte@bricodepot.com>
2015-04-07 17:42:39 +0200

    (*) Suppression caractéres spéciaux du nom des branches (pour généré id du nom des span) (*)Mise à jour gitignore pour ne pas prendre en compte : 	- les fichiers nw.js pour une install windows 	- les fichiers spécifiques aux projets Eclipse

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 $(function() {
-  var JENKINS_URL =  "https://ci.jenkins-ci.org";
+  var JENKINS_URL =  "http://grdevbdfr2.dev.kf-sbfr.net:9087/jenkins";
   var LASTBUILD_URI = "/lastBuild/api/json?pretty=true";
   var LIST_ALL_JOB = "/api/json?pretty=true";
   var JOBS_PATH = "/view/All/job/";
   var br = $('<br>');
   $(document).ready(function() {
-     //////////////////////////
+	//////////////////////////
     //List all jobs
     ////////////////////////
     var jqxhrAllJobs = $.getJSON( JENKINS_URL+LIST_ALL_JOB, function(data) {
@@ -21,34 +21,45 @@ $(function() {
             href: data.url,
             id: a_lastBuild +job.name
           });
-          $("#span_" + job.name).append(a_lastBuild);
+          var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+          $(spanJobNameId).append(a_lastBuild);
           var divLastCommit = $('<div>',{
             id : "lastCommit" + job.name,
             class: "d_LastCommit"
-          }).appendTo($("#span_" + job.name));
+          }).appendTo($(spanJobNameId));
 
           $.each(data.changeSet.items,function(j,item){
             divLastCommit.append(item.comment).append(br);
           });
+      }).fail(function() {
+    	  var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+    	    console.log( "error");
+            // On finalise la ligne par un saut de ligne
+            $(spanJobNameId).append(br);
       });
       ///////////////////////////////////
       //Appeler à chaque fois que l'appel JSON est finalisé
       ///////////////////////////////////
       jqxhrAllJobs.complete(function() {
+    	  var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
         // On finalise la ligne par un saut de ligne
-        $("#span_" + job.name).append(br);
+        $(spanJobNameId).append(br);
       });
     });
   });
   });
-
+  suppSymbolFromBranch = function(value){
+		var re = /([\(-\)])/g; 
+		return value.replace(re,"-");
+	}
 
   displayOneJob = function (job){
     var icon = "./ressources/img/greenchecked.png";
     if (job.color !== "blue"){
       icon = "./ressources/img/redchecked.png";
     }
-    var span = $('<span>',{id: "span_" + job.name}).addClass('text_valign-middle');
+    var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+    var span = $('<span>',{id: spanJobNameId}).addClass('text_valign-middle');
     var img = $('<img>',{
       id: "icon_" +job.name,
       src: icon
@@ -63,3 +74,4 @@ $(function() {
     span.append(img).append(a_href).appendTo("#jobs");
   }
 })
+
