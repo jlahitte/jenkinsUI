454da48f9e8f31948dba7d7221739cf116406c4e
Johann LAHITTE <jlahitte@bricodepot.com>
2015-04-10 12:33:29 +0200

    (*) Mise en place fichier de configuration application   - Import module jsoncfg   - creation fichier de conf. jenkins.json   - Mise en place script chargement conf (init.js)

---
+++ scripts/init.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	var jsoncfg = require('jsoncfg');
+	jsoncfg.loadFiles('./cfg', 
+			function(err, files, errInfo) {
+				if (err){
+					console.log(err + "  " + errInfo['jenkins']);
+				}
+				var jenkinsFcfg = files.jenkins;
+				console.log("Conf file :" + jenkinsFcfg);
+				window.JENKINS_URL = jenkinsFcfg.get('jenkins.url');
+				//lancement du traitement
+				main();
+			});
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,77 @@
-$(function() {
-  var JENKINS_URL =  "http://grdevbdfr2.dev.kf-sbfr.net:9087/jenkins";
-  var LASTBUILD_URI = "/lastBuild/api/json?pretty=true";
-  var LIST_ALL_JOB = "/api/json?pretty=true";
-  var JOBS_PATH = "/view/All/job/";
-  var br = $('<br>');
-  $(document).ready(function() {
-	//////////////////////////
-    //List all jobs
-    ////////////////////////
-    var jqxhrAllJobs = $.getJSON( JENKINS_URL+LIST_ALL_JOB, function(data) {
-    $.each(data.jobs, function(i, job){
-
-      displayOneJob(job);
-      ///////////////////////////////////
-      //Récupération du dernier build
-      //////////////////////////////////
-      var jqxhrAllJobs = $.getJSON( JENKINS_URL+ JOBS_PATH + job.name+ LASTBUILD_URI, function(data) {
-          var a_lastBuild = $('<a>',{
-            text: data.displayName,
-            href: data.url,
-            id: a_lastBuild +job.name
-          });
-          var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
-          $(spanJobNameId).append(a_lastBuild);
-          var divLastCommit = $('<div>',{
-            id : "lastCommit" + job.name,
-            class: "d_LastCommit"
-          }).appendTo($(spanJobNameId));
-
-          $.each(data.changeSet.items,function(j,item){
-            divLastCommit.append(item.comment).append(br);
-          });
-      }).fail(function() {
-    	  var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
-    	    console.log( "error");
-            // On finalise la ligne par un saut de ligne
-            $(spanJobNameId).append(br);
-      });
-      ///////////////////////////////////
-      //Appeler à chaque fois que l'appel JSON est finalisé
-      ///////////////////////////////////
-      jqxhrAllJobs.complete(function() {
-    	  var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
-        // On finalise la ligne par un saut de ligne
-        $(spanJobNameId).append(br);
-      });
-    });
-  });
-  });
-  suppSymbolFromBranch = function(value){
-		var re = /([\(-\)])/g; 
-		return value.replace(re,"-");
-	}
-
-  displayOneJob = function (job){
-    var icon = "./ressources/img/greenchecked.png";
-    if (job.color !== "blue"){
-      icon = "./ressources/img/redchecked.png";
-    }
-    var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
-    var span = $('<span>',{id: spanJobNameId}).addClass('text_valign-middle');
-    var img = $('<img>',{
-      id: "icon_" +job.name,
-      src: icon
-    });
-    var a_href = $('<a>',{
-        id: "a_" + job.name,
-        text: job.name,
-        title: job.name,
-        href: job.url
-    }).addClass('l_jobs');
-
-    span.append(img).append(a_href).appendTo("#jobs");
-  }
+$( 
+		main = function() {
+		  var LASTBUILD_URI = "/lastBuild/api/json?pretty=true";
+		  var LIST_ALL_JOB = "/api/json?pretty=true";
+		  var JOBS_PATH = "/view/All/job/";
+		  var br = $('<br>');
+		  $(document).ready(function() {
+			//////////////////////////
+		    //List all jobs
+		    ////////////////////////
+		    var jqxhrAllJobs = $.getJSON( JENKINS_URL+LIST_ALL_JOB, function(data) {
+		    $.each(data.jobs, function(i, job){
+		
+		      displayOneJob(job);
+		      ///////////////////////////////////
+		      //Récupération du dernier build
+		      //////////////////////////////////
+		      var jqxhrAllJobs = $.getJSON( JENKINS_URL+ JOBS_PATH + job.name+ LASTBUILD_URI, function(data) {
+		          var a_lastBuild = $('<a>',{
+		            text: data.displayName,
+		            href: data.url,
+		            id: a_lastBuild +job.name
+		          });
+		          var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+		          $(spanJobNameId).append(a_lastBuild);
+		          var divLastCommit = $('<div>',{
+		            id : "lastCommit" + job.name,
+		            class: "d_LastCommit"
+		          }).appendTo($(spanJobNameId));
+		
+		          $.each(data.changeSet.items,function(j,item){
+		            divLastCommit.append(item.comment).append(br);
+		          });
+		      }).fail(function() {
+		    	  var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+		    	    console.log( "error");
+		            // On finalise la ligne par un saut de ligne
+		            $(spanJobNameId).append(br);
+		      });
+		      ///////////////////////////////////
+		      //Appeler à chaque fois que l'appel JSON est finalisé
+		      ///////////////////////////////////
+		      jqxhrAllJobs.complete(function() {
+		    	  var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+		        // On finalise la ligne par un saut de ligne
+		        $(spanJobNameId).append(br);
+		      });
+		    });
+		  });
+		  });
+		  suppSymbolFromBranch = function(value){
+				var re = /([\(-\)])/g; 
+				return value.replace(re,"-");
+			}
+		
+		  displayOneJob = function (job){
+		    var icon = "./ressources/img/greenchecked.png";
+		    if (job.color !== "blue"){
+		      icon = "./ressources/img/redchecked.png";
+		    }
+		    var spanJobNameId = "#span_" + suppSymbolFromBranch(job.name);
+		    var span = $('<span>',{id: spanJobNameId}).addClass('text_valign-middle');
+		    var img = $('<img>',{
+		      id: "icon_" +job.name,
+		      src: icon
+		    });
+		    var a_href = $('<a>',{
+		        id: "a_" + job.name,
+		        text: job.name,
+		        title: job.name,
+		        href: job.url
+		    }).addClass('l_jobs');
+		
+		    span.append(img).append(a_href).appendTo("#jobs");
+		  }
 })
 
