7cb9833f458f1cc33efe21aa189048fb6b74e439
Johann LAHITTE <jlahitte@bricodepot.com>
2015-06-29 18:04:47 +0200

    Correction probleme fichier config branches

--- scripts/init.js
+++ scripts/init.js
@@ -8,17 +8,17 @@ $(document).ready(function(){
 				var jenkinsFcfg = files.jenkins;
 				console.log("Conf file :" + jenkinsFcfg);
 				window.JENKINS_URL = jenkinsFcfg.get('jenkins.url');
-				var branches = files.branchs;
-				window.JENKINS_BRANCHES = [branches.environnements.DR1,
-										   branches.environnements.IR1,
-										   branches.environnements.PP2,
-										   branches.environnements.PRD];
-				
 
 				var githubFcfg = files.gitHub;
 				console.log("Conf file :" + githubFcfg);
 				window.GITHUB_URL = githubFcfg.get('github.url');
 				window.GITHUB_TOKEN = githubFcfg.get('github.access_token');
+				
+				var branches = files.branches;
+				window.JENKINS_BRANCHES = [branches.environnements.DR1,
+										   branches.environnements.IR1,
+										   branches.environnements.PP2,
+										   branches.environnements.PRD];
 				
 				//lancement du traitement
 				connectToJenkins();
